03562cfd1b6c45f163fa5d3a24ea0ebd7ca217bf
conwabueze <chinonso1nwabueze@gmail.com>
2020-12-27 18:57:57 -0500

    Footer dt/mb complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/shared/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Footer from './components/shared/Footer/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
+      <Footer />
     </div>
   );
 }
