06134d39f2bbf68f3f9ab057cc00f4b8cdb1c5d3
conwabueze <chinonso1nwabueze@gmail.com>
2020-12-27 17:31:25 -0500

    Navbar Mobile complete

--- frontend/src/components/shared/Navbar/Navbar.js
+++ frontend/src/components/shared/Navbar/Navbar.js
@@ -10,32 +10,78 @@ class Navbar extends Component {
 
     this.state = {
       activeDropdown: '',
+      activateDropdown: true,
+      mobileNavOpen: false,
     };
 
     this.sneakerLinksMouseOver = this.sneakerLinksMouseOver.bind(this);
     this.sneakerLinksMouseLeave = this.sneakerLinksMouseLeave.bind(this);
+    this.toggleMobileNav = this.toggleMobileNav.bind(this);
   }
 
+  //trigger deactivateSneakerLinkDropdown() which will deativate the sneakerLinks dropdown for Desktop if we are in mobile
+  componentDidMount() {
+    this.deactivateSneakerLinkDropdown();
+    window.addEventListener(
+      'resize',
+      this.deactivateSneakerLinkDropdown.bind(this)
+    );
+  }
+
+  //remove deactivateSneakerLinkDropdown() resize event listener on unmount
+  componentWillUnmount() {
+    window.removeEventListener(
+      'resize',
+      this.deactivateSneakerLinkDropdown.bind(this)
+    );
+  }
+
+  //function determines which dropdown information to dropdown in Desktop when hovering over Sneakerlinks
   sneakerLinksMouseOver(e) {
     if (e.target.classList[0] === 'Navbar-sneaker-link') {
       this.setState({ activeDropdown: e.target.classList[1] });
     }
   }
 
+  //function hides dropdown in desktop when you over mouse away from Sneakerlinks
   sneakerLinksMouseLeave(e) {
-    if (e.target.parentNode.className === 'Navbar-sneaker-links') {
+    if (e.target.parentNode.classList[0] === 'Navbar-sneaker-links') {
       this.setState({ activeDropdown: '' });
     }
   }
 
-  dropdownRender() {
-    if (this.state.activeDropdown === '') {
+  //function checks window width to see if its mobile size and sets the state to deactivate the SneakerDropdown component
+  //when in mobile
+  deactivateSneakerLinkDropdown() {
+    if (window.innerWidth <= 970) this.setState({ activateDropdown: false });
+    else this.setState({ activateDropdown: true });
+  }
+
+  //render dropdown for Desktop when you hover over Sneakerlink
+  renderSneakerLinkDropdown() {
+    if (
+      this.state.activeDropdown === '' ||
+      this.state.activateDropdown === false
+    ) {
       return '';
     }
     return <SneakerDropdown dropdownType={this.state.activeDropdown} />;
   }
 
+  // Open/close mobile nav when you click on hamburger button
+  toggleMobileNav() {
+    this.state.mobileNavOpen === true
+      ? this.setState({ mobileNavOpen: false })
+      : this.setState({ mobileNavOpen: true });
+  }
+
   render() {
+    const displayCartAccountLinks = this.state.mobileNavOpen
+      ? 'cart-account-links-open-nav'
+      : '';
+    const displaySneakerLinks = this.state.mobileNavOpen
+      ? 'sneaker-links-open-nav'
+      : '';
     return (
       <div className="Navbar">
         <ContentContainer>
@@ -43,8 +89,12 @@ class Navbar extends Component {
             Jonah's Place
           </Link>
 
+          <button className="Navbar-hamburger" onClick={this.toggleMobileNav}>
+            {String.fromCharCode(9776)}
+          </button>
+
           <div
-            className="Navbar-sneaker-links"
+            className={`Navbar-sneaker-links ${displaySneakerLinks}`}
             onMouseOver={this.sneakerLinksMouseOver}
             onMouseLeave={this.sneakerLinksMouseLeave}
           >
@@ -72,9 +122,11 @@ class Navbar extends Component {
             >
               New Balance
             </Link>
-            {this.dropdownRender()}
+            {this.renderSneakerLinkDropdown()}
           </div>
-          <div className="Navbar-cart-account-links">
+          <div
+            className={`Navbar-cart-account-links ${displayCartAccountLinks}`}
+          >
             <Link to="#" className="Navbar-cart-account-link Navbar-cart-link">
               <i className="fas fa-shopping-cart fa-sm" />
               <p className="cart-text">Cart</p>
