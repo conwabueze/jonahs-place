b025c6ce8f195aa931aec593e63807fab79a3589
conwabueze <chinonso1nwabueze@gmail.com>
2021-01-24 23:17:05 -0500

    Login form complete

--- frontend/src/components/Account/Account.js
+++ frontend/src/components/Account/Account.js
@@ -4,7 +4,7 @@ import Login from './Login';
 import AccountSettings from './AccountSettings';
 import ContentContainer from '../shared/ContentContainer/ContentContainer';
 import axios from 'axios';
-const apiUrl = 'https://jonahsplace.herokuapp.com/api/v1';
+//const apiUrl = 'https://jonahsplace.herokuapp.com/api/v1';
 
 //http://localhost:3001/api/v1
 class Account extends Component {
@@ -14,16 +14,27 @@ class Account extends Component {
     this.state = {
       loginEmailInput: '',
       loginPasswordInput: '',
+      loginError: false,
       redirect: null,
       userName: this.props.user.name,
       userEmail: this.props.user.email,
-      userPassword: '',
-      userNewPassword: '',
-      userConfirmPassword: '',
+      userPassword: 'a',
+      userNewPassword: 'a',
+      userConfirmPassword: 'a',
     };
 
     this.handleAccountInputs = this.handleAccountInputs.bind(this);
     this.loginSubmit = this.loginSubmit.bind(this);
+    this.editFormSubmit = this.editFormSubmit.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.user === '' && this.props.user !== '') {
+      this.setState({
+        userName: this.props.user.name,
+        userEmail: this.props.user.email,
+      });
+    }
   }
 
   //update state in relation to whats being typed in the input fields
@@ -34,19 +45,67 @@ class Account extends Component {
   //action after you click on Login in
   async loginSubmit(e) {
     e.preventDefault();
-    const res = await axios.post(
-      `http://localhost:3001/api/v1/users/login`,
-      {
-        email: this.state.loginEmailInput,
-        password: this.state.loginPasswordInput,
-      },
+    try {
+      const res = await axios.post(
+        `http://localhost:3001/api/v1/users/login`,
+        {
+          email: this.state.loginEmailInput,
+          password: this.state.loginPasswordInput,
+        },
+        { withCredentials: true }
+      );
+
+      if (res.status === 200) {
+        this.props.appRerender();
+        this.setState({ redirect: '/' });
+      }
+    } catch (err) {
+      console.log('error');
+      this.setState({ loginError: true });
+      window.setTimeout(() => this.setState({ loginError: false }), 3000);
+    }
+
+    // console.log('fjkdsn');
+    // if (res) {
+    //   console.log('chip');
+    //   this.setState({ loginError: true });
+    // }
+  }
+
+  //action when you click on edit button in accountSettings
+  async editFormSubmit(e) {
+    e.preventDefault();
+    const res = await axios.patch(
+      'http://localhost:3001/api/v1/users/updateMe',
+      { name: this.state.userName, email: this.state.userEmail },
       { withCredentials: true }
     );
 
-    if (res) {
-      this.props.appRerender();
-      this.setState({ redirect: '/' });
+    if (res.status === 200) {
+      window.location.reload();
     }
+  }
+
+  //action when you click on change password button in accountSettings
+  async changePasswordSubmit(e) {
+    e.preventDefault();
+    try {
+      console.log(this.state);
+      const res = await axios.patch(
+        'http://localhost:3001/api/v1/users/updateMyPassword',
+        {
+          currentPasssword: this.state.userPassword,
+          newPassword: this.state.userNewPassword,
+          newPasswordConfirm: this.state.userConfirmPassword,
+        },
+        { withCredentials: true }
+      );
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+
+    console.log('password submit clicked');
   }
 
   renderCorrectComponent() {
@@ -56,12 +115,27 @@ class Account extends Component {
           handleAccountInputs={this.handleAccountInputs}
           loginEmailInput={this.state.loginEmailInput}
           loginPasswordInput={this.state.loginPasswordInput}
+          loginError={this.state.loginError}
           loginSubmit={this.loginSubmit}
           redirect={this.state.redirect}
         />
       );
     } else if (this.props.renderSettings) {
-      return <AccountSettings user={this.props.user} />;
+      return (
+        <AccountSettings
+          userTitle={this.props.user.name}
+          userName={this.state.userName}
+          userEmail={this.state.userEmail}
+          userPassword={this.state.userPassword}
+          userNewPassword={this.state.userNewPassword}
+          userConfirmPassword={this.state.userConfirmPassword}
+          handleAccountInputs={this.handleAccountInputs}
+          editFormSubmit={this.editFormSubmit}
+          appRerender={this.props.appRerender}
+          changePasswordSubmit={this.changePasswordSubmit}
+          redirect={this.state.redirect}
+        />
+      );
     }
   }
 
--- frontend/src/components/Account/AccountSettings.js
+++ frontend/src/components/Account/AccountSettings.js
@@ -1,40 +1,116 @@
 import { Component } from 'react';
 import './AccountSettings.css';
+import { Redirect } from 'react-router-dom';
 import ContentContainer from '../shared/ContentContainer/ContentContainer';
-import axios from 'axios';
-const apiUrl = 'https://jonahsplace.herokuapp.com/api/v1';
 
 class AccountSettings extends Component {
   render() {
+    const renderRedirect =
+      this.props.redirect !== null ? <Redirect to={this.props.redirect} /> : '';
     return (
       <div className="AccountSettings">
         <ContentContainer>
-          <h1 className="AccountSettings-header">{`Hi, ${
-            this.props.user.name.split(' ')[0]
-          }`}</h1>
+          <h1 className="AccountSettings-header">{`Hi, ${this.props.userTitle}`}</h1>
           <h2 className="AccountSettings-header2">
             here are your account settings
           </h2>
-          <form className="AccountSettings-form AccountSettings-edit-form">
-            <label for="name">name</label>
+          <form
+            onSubmit={this.props.editFormSubmit}
+            className="AccountSettings-form AccountSettings-edit-form"
+          >
+            <label className="AccountSettings-label" htmlFor="name">
+              name
+            </label>
             <br></br>
             <input
               className="AccountSettings-input AccountSettings-edit-input"
               type="text"
-              id="name"
-              name="name"
-              value={this.props.user.name}
+              name="userName"
+              minLength="2"
+              pattern="^[A-Za-z]+$"
+              value={this.props.userName}
+              onChange={this.props.handleAccountInputs}
+              required
             />
             <br></br>
-            <label for="email">email</label>
+            <label className="AccountSettings-label" htmlFor="email">
+              email
+            </label>
             <br></br>
             <input
               className="AccountSettings-input AccountSettings-edit-input"
               type="email"
-              id="email"
-              name="email"
-              value={this.props.user.email}
+              name="userEmail"
+              value={this.props.userEmail}
+              onChange={this.props.handleAccountInputs}
             />
+            <br></br>
+            <button
+              className="AccountSettings-submit AccountSettings-edit-submit"
+              type="submit"
+            >
+              Edit
+            </button>
+            {renderRedirect}
+          </form>
+
+          <div className="AccountSettings-line-break"></div>
+          <h2 className="AccountSettings-header2">
+            want to change your password?
+          </h2>
+
+          <form
+            className="AccountSettings-form AccountSettings-password-form"
+            onSubmit={this.props.changePasswordSubmit}
+          >
+            <label className="AccountSettings-label" htmlFor="userPassword">
+              current password
+            </label>
+            <br></br>
+            <input
+              className="AccountSettings-input AccountSettings-password-input"
+              type="password"
+              name="userPassword"
+              minLength="8"
+              value={this.props.userPassword}
+              onChange={this.props.handleAccountInputs}
+            />
+            <br></br>
+            <label className="AccountSettings-label" htmlFor="userNewPassword">
+              new password
+            </label>
+            <br></br>
+            <input
+              className="AccountSettings-input AccountSettings-password-input"
+              type="password"
+              name="userNewPassword"
+              minLength="8"
+              value={this.props.userNewPassword}
+              onChange={this.props.handleAccountInputs}
+            />
+            <br></br>
+            <label
+              className="AccountSettings-label"
+              htmlFor="userConfirmPassword"
+            >
+              confirm password
+            </label>
+            <br></br>
+            <input
+              className="AccountSettings-input AccountSettings-password-input"
+              type="password"
+              name="userConfirmPassword"
+              minLength="8"
+              value={this.props.userConfirmPassword}
+              onChange={this.props.handleAccountInputs}
+            />
+            <br></br>
+            <button
+              className="AccountSettings-submit AccountSettings-passsword-submit"
+              type="submit"
+            >
+              change password
+            </button>
           </form>
         </ContentContainer>
       </div>
