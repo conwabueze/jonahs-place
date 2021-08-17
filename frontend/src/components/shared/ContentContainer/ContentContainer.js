14306fcc298e39358c95342c0f3685764d61face
conwabueze <chinonso1nwabueze@gmail.com>
2021-08-17 17:50:54 -0400

    frontend alter

--- frontend/src/components/shared/ContentContainer/ContentContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './ContentContainer.css';
-
-function ContentContainer(props) {
-  return <div className="ContentContainer"> {props.children}</div>;
-}
-
-export default ContentContainer;
